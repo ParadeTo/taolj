049cc163ea58ae35b6a81b6f282177a2c4d74eea
youxingzhi <xingzhi.you@shopee.com>
2023-01-29 14:50:42 +0800

    item server mysql

--- item-server/src/app.module.ts
+++ item-server/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { ItemService } from './item.service';
 import { ItemController } from './item.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '',
+      database: 'item',
+      entities: [],
+      synchronize: true
+    })
+  ],
+
   controllers: [ItemController],
   providers: [ItemService]
 })
